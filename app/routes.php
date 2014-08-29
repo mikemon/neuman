d7c8ec17e390e912e44ee357c6e203fac8413c3c
unknown <EQUIPO11@.(none)>
2014-08-29 15:07:16 -0500

    index para redireccionar

--- app/routes.php
+++ app/routes.php
@@ -10,10 +10,17 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
-
+/*
 Route::get('/', function()
 {
 	return View::make('hello');
+});
+*/
+Route::get('/', function()
+{
+	return Redirect::to('home');
+	//exit;
+	//return View::make('hello');
 });
 Route::get('usuarios', array('uses' => 'UsuariosController@mostrarUsuarios'));
  
